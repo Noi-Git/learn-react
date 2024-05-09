bc26614837771b2290baa91c75f5c62ca295babe
Noi-Git <noi.patsin@gmail.com>
2024-05-09 00:24:49 -0400

    add basic backend get route

--- search-api/api/index.js
+++ search-api/api/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 const app = express()
 import cors from 'cors'
+import { Users } from './users.js'
 
 app.use(cors())
 
-app.get('/', (req, res) => {})
+app.get('/', (req, res) => {
+  res.json(User)
+})
 
-app.listen(5000, () => console.log('Server is listening on port 5000'))
+app.listen(5001, () => console.log('Server is listening on port 5001'))
